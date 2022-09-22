7f5cc76e678e58d6ffc9af2149b45886bcb5c6cf
BDFD <bdfd2005@gmail.com>
2022-09-22 18:21:08 -0400

    update section one and section two

--- static/javascript/SectionContent.js
+++ static/javascript/SectionContent.js
@@ -2,7 +2,7 @@
  * @Date         : 2022-09-22 17:06:54
  * @Author       : BDFD,bdfd2005@gmail.com
  * @Github       : https://github.com/bdfd
- * @LastEditTime : 2022-09-22 18:01:10
+ * @LastEditTime : 2022-09-22 18:07:00
  * @LastEditors  : BDFD
  * @Description  :
  * @FilePath     : \static\javascript\SectionContent.js
@@ -10,6 +10,10 @@
  */
 $(function () {
 	$("#Navigation").load("./templates/Navigation.html");
+	$("#SectionOne").load("./templates/Section01.html");
+	$("#SectionTwo").load("./templates/Section02.html");
+	$("#SectionThree").load("./templates/Section03.html");
+	// $("#SectionTwo").load("./templates/SectionTwo.html");
 	// $("#Navigation").load("./templates/Navigation.html");
 });
 console.log("Hello World!");
