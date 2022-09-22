13e6fdc26c225ddc6b1e3549ecd69b5d8a2c9f06
BDFD <bdfd2005@gmail.com>
2022-09-22 17:25:07 -0400

    Init Commit

---
+++ static/javascript/SectionContent.js
@@ -0,0 +1,13 @@
+/*
+ * @Date         : 2022-09-22 17:06:54
+ * @Author       : BDFD,bdfd2005@gmail.com
+ * @Github       : https://github.com/bdfd
+ * @LastEditTime : 2022-09-22 17:21:27
+ * @LastEditors  : BDFD
+ * @Description  :
+ * @FilePath     : \static\javascript\SectionContent.js
+ * Copyright (c) 2022 by BDFD, All Rights Reserved.
+ */
+$(function () {
+	$("#ContentToInclude").load("./Navigation.html");
+});
